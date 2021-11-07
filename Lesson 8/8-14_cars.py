ef6db9c3d56327a0dd34a2cac5576e73d1e989c6
Julka3561 <julka3561@icloud.com>
2021-11-07 12:43:07 +0300

    1

--- Lesson 8/8-14_cars.py
+++
@@ -1,9 +0,0 @@
-def make_car(manuf, model, **car_info):
-    car_info['manufacturer'] = manuf
-    car_info['model'] = model
-    return car_info
-
-car = make_car('skoda', 'yeti', color = 'silver', owner = 'dimkin')
-
-for i, v in car.items():
-    print(f'{i}: {v.title()}')
