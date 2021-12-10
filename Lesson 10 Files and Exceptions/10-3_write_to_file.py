671a82059fe833f472dd3989797ac2b3b4e9bc09
Юлия Васильева <julka3561@gmail.com>
2021-12-10 19:16:26 +0300

    wqwq`

---
+++ Lesson 10 Files and Exceptions/10-3_write_to_file.py
@@ -0,0 +1,8 @@
+print('Моя сила!!!')
+name = ''
+while name != 'q':
+    name = input('Введите имя:')
+    if name == 'Юля':
+        print(f'Добро пожаловать {name}')
+    else:
+        print('Bye...')
